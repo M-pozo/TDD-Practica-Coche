cbeb78624b47abeee50cbf7b23e0e89831eaac50
Miguel <m.pozo.2002@gmail.com>
2023-03-29 19:07:40 +0200

    Creamos variable velocidad y actualizamos testCrearCoche -> testCrearCocheVelocidadCero

--- test/TestCoche.java
+++ test/TestCoche.java
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void testCrearCoche(){
+    public void testCrearCocheVelocidadCero(){
         Coche nuevoCoche = new Coche();
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
 }
