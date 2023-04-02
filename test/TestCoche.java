e94ccaa21fbad95db428c2b5a0762a5e9bd7db66
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-04-02 19:53:43 +0200

    Creamos testDecelerarNoPuedeSerMEnorQueCero y actualizamos el metodo decelerar

--- test/TestCoche.java
+++ test/TestCoche.java
@@ -22,5 +22,12 @@ public class TestCoche {
         nuevoCoche.decelerar(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
+    @Test
+    public void testAlDecelerarCocheVelocidadNoPuedeSerMenorQueCero(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad = 50;
+        nuevoCoche.decelerar(80);
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
+    }
 
 }
