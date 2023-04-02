944236150325145cb04aca94b12f65e1e1ad6484
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-04-02 20:01:38 +0200

    Agregar nombre a cada metodo

--- test/TestCoche.java
+++ test/TestCoche.java
@@ -3,30 +3,30 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void testCrearCocheVelocidadCero(){
+    public void testCrearCocheVelocidadCeroMiguelPozo(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
     @Test
-    public void testAcelerarCocheVelocidadAumenta(){
+    public void testAcelerarCocheVelocidadAumentaMiguelPozo(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerarMiguelPozo(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
     @Test
-    public void testDecelerarCocheVelocidadDisminuye(){
+    public void testDecelerarCocheVelocidadDisminuyeMiguelPozo(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerarMiguelPozo(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void testAlDecelerarCocheVelocidadNoPuedeSerMenorQueCero(){
+    public void testAlDecelerarCocheVelocidadNoPuedeSerMenorQueCeroMiguelPozo(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerarMiguelPozo(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
